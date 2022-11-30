aa9fd5c0cafebcab66f14bfaaa30ebfda67fe463
AlexKurdelska <alex.kurdelska@gmail.com>
2022-11-30 10:44:00 +0100

    MP3 player

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,10 +1,14 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
 import java.io.IOException;
 
 
@@ -15,9 +19,18 @@ public class Main extends Application {
         primaryStage.setTitle("Application");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
     }
 
     public static void main(String[] args) {
         launch();
     }
+
 }
